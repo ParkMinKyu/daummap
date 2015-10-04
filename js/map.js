232a258e0179beb3d1ea99de12832401ac35205e
Park Min Kyu <niee@naver.com>
2015-10-04 17:32:55 +0900

    test

--- js/map.js
+++ js/map.js
@@ -405,29 +405,32 @@ function getTimeHTML(distance) {
 }
 
 // 지도에 표시된 마커 객체를 가지고 있을 배열입니다
-var markers = [];
+var markers = [][];
 
-function crateMarker(mapInfo, imgpath) {
-	var imageSrc = '/daummap'+imgpath, // 마커이미지의 주소입니다
-	imageSize = new daum.maps.Size(40, 40), // 마커이미지의 크기입니다
-	imageOption = {
-		offset : new daum.maps.Point(10, 40)
-	}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-	// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-	var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize,
-			imageOption), markerPosition = new daum.maps.LatLng(mapInfo.lat, mapInfo.lng); // 마커가
-																			// 표시될
-																			// 위치입니다
-	// 마커를 생성합니다
-	var marker = new daum.maps.Marker({
-		position : markerPosition,
-		image : markerImage
-	// 마커이미지 설정
-	});
-	// 마커가 지도 위에 표시되도록 설정합니다
-	marker.setMap(map);
-	if(isPush)
-		markers.push(marker);
+function crateMarker( lat, lng, imgpath) {
+	if(markers[mapCodeGroup.index][codeMapList.index] == null){
+		var imageSrc = '/daummap'+imgpath, // 마커이미지의 주소입니다
+		imageSize = new daum.maps.Size(40, 40), // 마커이미지의 크기입니다
+		imageOption = {
+			offset : new daum.maps.Point(10, 40)
+		}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+		// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+		var markerImage = new daum.maps.MarkerImage(imageSrc, imageSize,
+				imageOption), markerPosition = new daum.maps.LatLng(lat, lng); // 마커가
+																				// 표시될
+																				// 위치입니다
+		// 마커를 생성합니다
+		var marker = new daum.maps.Marker({
+			position : markerPosition,
+			image : markerImage
+		// 마커이미지 설정
+		});
+		// 마커가 지도 위에 표시되도록 설정합니다
+		marker.setMap(map);
+		markers[mapCodeGroup.index][codeMapList.index] = marker;
+	}else{
+		markers[mapCodeGroup.index][codeMapList.index].setMap(null)
+	}
 }
 
 // 배열에 추가된 마커들을 지도에 표시하거나 삭제하는 함수입니다
